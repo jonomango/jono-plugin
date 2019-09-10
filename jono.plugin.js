17db17ea8e891b5b104249203e264d31c27acc4b
jono <26609800+jonomango@users.noreply.github.com>
2019-09-10 14:23:14 -0700

    added JonoUtils and some wrapper functions

--- jono.plugin.js
+++ jono.plugin.js
@@ -1,5 +1,6 @@
 //META{"name":"JonoPlugin"}*//
 
+// https://github.com/rauenzi/BetterDiscordApp/wiki/Creating-Plugins
 class JonoPlugin {
     getName() { 
         return "Jono's Plugin"; 
@@ -11,14 +12,48 @@ class JonoPlugin {
         return "1.0"; 
     }
     getAuthor() { 
-        return "Jono"; 
+        return "jono"; 
     }
 
-    load() { }
     start() {
         console.log("Jono's plugin starting!");
+        JonoUtils.setup();
     }
     stop() {
         console.log("Jono's plugin stopping!");
+    }
+}
+
+// wrapper around some BdApi functions
+const JonoUtils = {
+    setup: () => {
+        JonoUtils.MessageActions = BdApi.findModuleByProps("jumpToMessage", "_sendMessage");
+        JonoUtils.UserInfoStore = BdApi.findModuleByProps("getToken");
+        JonoUtils.UserStore = BdApi.findModuleByProps("getCurrentUser");
+    },
+
+    getToken: () => {
+        return JonoUtils.UserInfoStore.getToken();
+    },
+    getUser: (userid) => {
+        if (userid) {
+            return JonoUtils.UserStore.getUser(userid);
+        }
+
+        return JonoUtils.UserStore.getCurrentUser();
+    },
+
+    sendMessage: (channelid, content) => {
+        if (!content) {
+            return;
+        }
+
+        JonoUtils.MessageActions._sendMessage(channelid, { 
+            content,
+            tts: false
+        });
+    },
+    sendEmbed: (channelid) => {
+        // TODO: implemment
     }
 }
